72978122343f0b0d691dcb2bd07c5626a88d2e8a
Guilherme-Lobato <guilobato2002@gmail.com>
2024-01-09 16:10:26 -0300

    algumas alterações

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaComponent } from './components/lista/lista.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Lista', component: ListaComponent},
+  {path: 'Cadastro-Filme', component: CadastroComponent},
+  { path: '', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListaComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
